fc65d9471a6d6c5f14012299d540e1f51ab97dd6
Masood-B <masoodbasardien123@gmail.com>
2023-05-17 10:40:23 +0200

    first

---
+++ script/code.js
@@ -0,0 +1,19 @@
+    let timelineEvent1 = {
+        1346: `the plague`,
+        1564: `william shakespear`
+
+    }
+
+    console.log(timelineEvent1)
+
+    let timelineEvent2 = {
+        1989: `Internet`,
+        1837: `Queen Victoia`
+
+    }
+
+    console.log(timelineEvent2)
+
+    let timelineEvent = [timelineEvent1, timelineEvent2]
+    console.log(timelineEvent)
+    
